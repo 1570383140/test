d89d2a8a33b668d238025fc9ffcd593583fd5265
sundayswift <2107652428@qq.com>
2021-07-13 16:18:20 +0800

    first commit

---
+++ ATest/ATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATestApp.swift
+//  ATest
+//
+//  Created by Biggerlens on 2021/7/5.
+//
+
+import SwiftUI
+
+@main
+struct ATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
